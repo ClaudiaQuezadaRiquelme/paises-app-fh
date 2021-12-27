22b43dc9a0d58c1cf7e075a04a70c25ff8f643c2
Claudia Andrea Quezada Riquelme <cquezada@tisal.local>
2021-12-27 16:03:01 -0300

    get info about country from API

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
 
   ngOnInit(): void {
+    // this.activateRoute.params
+    //   .subscribe( ({ id }) => {
+    //     console.log('params', id);
+        
+    //     this.paisService.getPaisPorCodigo( id )
+    //       .subscribe( pais => {
+    //         console.log('pais', pais);
+            
+    //       })
+    //   });
+
+    this.activateRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorCodigo( id ) )
+      )
+      .subscribe( resp => {
+        console.log('resp', resp);
+      });
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -21,4 +21,9 @@ export class PaisService {
     const url = `${ this.apiUrl }/capital/${ termino }`;
     return this.http.get<Country[]>( url );
   }
+
+  getPaisPorCodigo( id: string ): Observable<Country> {
+    const url = `${ this.apiUrl }/alpha?codes=${ id }`;
+    return this.http.get<Country>( url );
+  }
 }
